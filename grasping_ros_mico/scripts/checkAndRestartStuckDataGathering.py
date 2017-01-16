9ff6037c56c900b3feb56acc5653d703788fd292
nehagarg <priyadarshini.neha@gmail.com>
2017-01-16 16:42:17 +0800

    Added monitoring of directory in vrep stuck process monitor script

--- grasping_ros_mico/scripts/checkAndRestartStuckDataGathering.py
+++ grasping_ros_mico/scripts/checkAndRestartStuckDataGathering.py
@@ -4,9 +4,14 @@ import datetime
 import time
 
 if len(sys.argv) > 1:
-    filename = sys.argv[1]
- 
+    filename_in = sys.argv[1]
+    
     while True:
+        filename = filename_in
+        if filename == 'cwd':
+            files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
+            filename = files[-1]
+            print filename
         s = os.stat(filename)
         a = datetime.datetime.now() - datetime.datetime.fromtimestamp(s.st_mtime)
         print a.total_seconds()
@@ -16,4 +21,4 @@ if len(sys.argv) > 1:
     
         time.sleep(600)
 else:
-    os.system("rosservice call /vrep/simRosSetIntegerSignal checkTip 1")
+     os.system("rosservice call /vrep/simRosSetIntegerSignal checkTip 1")
