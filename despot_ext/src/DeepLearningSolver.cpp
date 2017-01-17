9572903e2d53c1fc439e48651e65dd36505ca29c
nehagarg <priyadarshini.neha@gmail.com>
2017-01-17 15:35:21 +0800

    Added functionality to directly pass sequence to deep learning model

--- despot_ext/src/DeepLearningSolver.cpp
+++ despot_ext/src/DeepLearningSolver.cpp
@@ -43,7 +43,7 @@ ValuedAction DeepLearningSolver::Search() {
     
    // std::string cmd_string = "cd python_scripts/deepLearning ; python model.py " + to_string((int)((LearningModel*)model_)->GetStartStateIndex()) + "; cd - ;";
     
-    std::string cmd_string = ((LearningModel*)model_)->GetPythonExecutionString();
+    std::string cmd_string = ((LearningModel*)model_)->GetPythonExecutionString(history_);
     
     std::cout << "Before calling exec" << std::endl;
     std::string result = exec(cmd_string.c_str());
