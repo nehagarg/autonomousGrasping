b1446704b27ec760f8abf2fc5f8601c961c0ee6f
nehagarg <priyadarshini.neha@gmail.com>
2017-01-28 08:30:35 +0800

    Added scrit to download the source of packages for installation on ncl

---
+++ python_scripts/download_packages_from_apt.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+def main():
+    package_name = sys.argv[1]
+    main_command = 'sudo apt-get download $(apt-rdepends ' + package_name+ ' | grep -v "^ " '
+    os.system(main_command + ') > ' + package_name + '.log 2>&1' )
+    additional_command = ""
+    with open(package_name + '.log', 'r') as f:
+        for line in f:
+            ignore_name = line.split(' ')[7]
+            additional_command = additional_command + '| grep -v "' + ignore_name + '" '
+            
+    os.system(main_command + additional_command +  ')')
