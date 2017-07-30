6dd8f7235cc350053ced280e5611ecb5508cc724
nehagarg <priyadarshini.neha@gmail.com>
2017-07-30 17:01:46 +0800

    Separated stopped process removal so that processes are allocated faster

--- python_scripts/experiment_v3.py
+++ python_scripts/experiment_v3.py
@@ -467,9 +467,10 @@ def check_finished_processes_standalone(running_node_file, stopped_node_file):
     update_running_nodes(running_node_file, running_nodes_to_screen, running_screen_to_nodes)
     update_running_nodes(stopped_node_file, stopped_nodes_to_screen, stopped_screen_to_nodes)
     while(not all_processes_stopped()):
+            
+            check_finished_processes(stopped_node_file)
             print "Sleeping before checking process status..."
             run_command_on_node('sleep 600')
-            check_finished_processes(stopped_node_file)
             update_running_nodes(running_node_file, running_nodes_to_screen, running_screen_to_nodes)
             #update_running_nodes(stopped_node_file, stopped_nodes_to_screen, stopped_screen_to_nodes)
             
