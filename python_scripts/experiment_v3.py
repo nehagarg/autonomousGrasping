5e285c952fa22e7f2020f6ee8000611e61c475fb
nehagarg <priyadarshini.neha@gmail.com>
2017-11-15 13:06:36 +0800

    Added use of generic vrep scene

--- python_scripts/experiment_v3.py
+++ python_scripts/experiment_v3.py
@@ -41,7 +41,7 @@ def generate_despot_command(t, n, l, c, problem_type, pattern, begin_index, end_
     if(command_prefix == 'gather_data'):
         #num = get_gather_data_number(pattern, t)
         command = './bin/gather_data ' + pattern + ' ' + t + ' ' + n
-        command = command + ' ' + ",".join(map(std,[begin_index, end_index, -1, -1]))
+        command = command + ' ' + ",".join(map(str,[begin_index, end_index, -1, -1]))
         return command
     actual_command = ' python ../python_scripts/experiment_v2.py -e -p ' + problem_type
     actual_command = actual_command + ' -s ' + repr(begin_index)
