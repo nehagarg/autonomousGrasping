1633c7f95438819e655c2537c06dd6290efd0f71
nehagarg <priyadarshini.neha@gmail.com>
2018-01-12 17:26:20 +0800

    Added code to sample g3db objects for training. Updated training and validation sets

--- python_scripts/grasping_object_list.py
+++ python_scripts/grasping_object_list.py
@@ -52,15 +52,22 @@ def get_grasping_object_name_list(type='used'):
         
     return pattern_list
 
-def get_g3db_instances(type = 'all'):
+def get_g3db_txt_file_path(server_name = 'ncl'):
     try:
         import rospkg
         rospack = rospkg.RosPack()
         grasping_ros_mico_path = rospack.get_path('grasping_ros_mico')
     except:
         #give ncl absolute path
-        grasping_ros_mico_path = '/users/ngarg211/WORK_FOLDER/neha_github/autonomousGrasping/grasping_ros_mico'
+        if(server_name == 'unicorn'):
+            grasping_ros_mico_path = '/data/neha/WORK_FOLDER/neha_github/autonomousGrasping/grasping_ros_mico'
+        else:
+            grasping_ros_mico_path = '/users/ngarg211/WORK_FOLDER/neha_github/autonomousGrasping/grasping_ros_mico'
     g3db_object_list_file = grasping_ros_mico_path + "/g3db_object_labels/object_instances/object_instances_updated/"
+    return g3db_object_list_file
+
+def get_g3db_instances(type = 'all'):
+    g3db_object_list_file = get_g3db_txt_file_path()
     if(type == 'all'):
        g3db_object_list_file = g3db_object_list_file +  "object_instance_names.txt"
     else:
@@ -71,3 +78,144 @@ def get_g3db_instances(type = 'all'):
         lines = f.readlines()
         ans = [l.strip() for l in lines]
     return ans
+
+def get_classes_from_g3db_instances(g3db_instances):
+    g3db_classes = sorted(list(set([x.split('-')[0] for x in g3db_instances])))
+    return g3db_classes
+    
+    
+def classify_g3db_instances():
+    ans = {}
+    g3db_object_cylindrical = []
+    g3db_object_square = []
+    g3db_object_handles = []
+    g3db_object_wine = []
+    g3db_object_stands = []
+    g3db_object_misc = []
+    g3db_object_square.append('104_toaster_final')
+    g3db_object_cylindrical.append('106_urn_final')
+    g3db_object_misc.append('109_crab_final')
+    g3db_object_misc.append('11_cheese_final')
+    g3db_object_square.append('18_garbage_box_final')
+    g3db_object_handles.append('1_Coffeecup_final')
+    g3db_object_cylindrical.append('24_bowl')
+    g3db_object_handles.append('24_mug_final')
+    g3db_object_cylindrical.append('25_mug')
+    g3db_object_misc.append('28_Spatula_final')
+    g3db_object_cylindrical.append('30_fruit_juicer_final')
+    g3db_object_cylindrical.append('39_beerbottle_final')
+    g3db_object_stands.append('40_carafe_final')
+    g3db_object_cylindrical.append('41_jar_and_lid_final')
+    g3db_object_wine.append('42_wineglass_final')
+    g3db_object_wine.append('43_wineglass2_final')
+    g3db_object_stands.append('44_towelstand_final')
+    g3db_object_cylindrical.append('48_bottle_and_plug_final')
+    g3db_object_cylindrical.append('49_apple_final')
+    g3db_object_square.append('50_carton_final')
+    g3db_object_cylindrical.append('52_jar2_final')
+    g3db_object_misc.append('54_candlestick_final')
+    g3db_object_misc.append('56_headphones_final')
+    g3db_object_cylindrical.append('5_bottle_final')
+    g3db_object_misc.append('62_mouse_final')
+    g3db_object_handles.append('63_candle_final')
+    g3db_object_cylindrical.append('65_coffeemaker_final')
+    g3db_object_square.append('66_chocolatebox_final')
+    g3db_object_cylindrical.append('67_jug_final')
+    g3db_object_cylindrical.append('6_jar_final')
+    g3db_object_square.append('73_juicebottle_final')
+    g3db_object_square.append('74_lamp_final')
+    g3db_object_misc.append('75_vase3_final')
+    g3db_object_wine.append('76_mirror_final')
+    g3db_object_misc.append('77_napkinholder_final')
+    g3db_object_misc.append('79_toy_dog_final')
+    g3db_object_cylindrical.append('80_tincan_final')
+    g3db_object_cylindrical.append('84_yogurtcup_final')
+    g3db_object_square.append('86_bread_final')
+    g3db_object_cylindrical.append('91_peppershaker_final')
+    g3db_object_misc.append('92_shell_final')
+    g3db_object_cylindrical.append('94_weight_final')
+    ans['cylindrical'] =g3db_object_cylindrical 
+    ans['square'] =g3db_object_square
+    ans['handles'] =g3db_object_handles 
+    ans['wine'] =g3db_object_wine
+    ans['stands'] =g3db_object_stands 
+    ans['misc'] =g3db_object_misc
+    return ans
+
+def get_object_classification(non_test_object_classes):
+    g3db_object_classification = classify_g3db_instances()
+    ans = {}
+    for object_class_category in g3db_object_classification.keys():
+        ans[object_class_category] = []
+        for object_class in g3db_object_classification[object_class_category]:
+            if object_class in non_test_object_classes:
+                ans[object_class_category].append(object_class)
+    return ans
+def test_get_object_classification():
+    g3db_instances = get_g3db_instances()
+    g3db_instance_classes = get_classes_from_g3db_instances(g3db_instances)
+    g3db_object_classification = get_object_classification(g3db_instance_classes)
+    g3db_instances_classified = []
+    for object_class_category in g3db_object_classification.keys():
+        g3db_instances_classified = g3db_instances_classified + g3db_object_classification[object_class_category]
+    print len(g3db_instances_classified)
+    print len(g3db_instance_classes)
+    assert(len(g3db_instances_classified) == len(g3db_instance_classes))
+  
+def sample_object_instances(num_samples, object_instance_list):
+    import random
+    a = range(0,len(object_instance_list))
+    random.shuffle(a)
+    ans=[]
+    for i in range(0,num_samples):
+        ans.append(object_instance_list[a[i]])
+    return ans
+
+def get_instances_for_class(object_class_list, type):
+    object_instances = get_g3db_instances(type)
+    target_object_instances = []
+    for object_class in object_class_list:
+        target_object_instances = target_object_instances + [x  for x in object_instances  if object_class in x]
+    return target_object_instances
+
+def sample_instances_from_class(num_samples, object_class_list, type):
+    target_object_instances = get_instances_for_class(object_class_list, type)
+    ans = sample_object_instances(num_samples, target_object_instances)
+    return ans
+    
+def sample_one_from_class(num_samples, object_class_list, type): 
+    ans = []
+    square_classes = sample_object_instances(num_samples, object_class_list)
+    for square_class in square_classes:
+        ans = ans + sample_instances_from_class(1, [square_class], type)
+    return ans
+
+def sample_from_non_test_g3db_instances(update_file = False):
+    non_test_object_instances = get_g3db_instances('non_test')
+    non_test_object_classes = get_classes_from_g3db_instances(non_test_object_instances)
+    #print non_test_object_classes
+    non_test_object_classification = get_object_classification(non_test_object_classes)
+    #print non_test_object_classification
+    ans = []
+    ans = ans + sample_one_from_class(4, non_test_object_classification['cylindrical'], 'non_test')
+    ans = ans + sample_one_from_class(2, non_test_object_classification['square'], 'non_test')
+    ans = ans + sample_instances_from_class(2, non_test_object_classification['handles'], 'non_test')
+    ans = ans + sample_instances_from_class(1, non_test_object_classification['wine'], 'non_test')
+    ans = ans + sample_instances_from_class(2, non_test_object_classification['stands'], 'non_test')
+    ans = ans + sample_one_from_class(4, non_test_object_classification['misc'], 'non_test')
+    
+    print ans
+    validation_set = sorted(list(set(non_test_object_instances) - set(ans)))
+    if update_file:
+        g3db_object_list_file = get_g3db_txt_file_path()
+        g3db_train_file_name = g3db_object_list_file +  "object_instance_names_" + "train1" + ".txt"
+        with open(g3db_train_file_name, 'w') as f:
+            f.write("\n".join(ans))
+            f.write("\n")
+        g3db_validation_file_name = g3db_object_list_file +  "object_instance_names_" + "validation1" + ".txt"
+        with open(g3db_validation_file_name, 'w') as f:
+            f.write("\n".join(validation_set))
+            f.write("\n")
+    
+    
+    
