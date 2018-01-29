ee15e2af40c749fa697dba39d96fa3129b3395e3
nehagarg <priyadarshini.neha@gmail.com>
2018-01-29 11:18:50 +0800

    Updated dirs in combine csvs

--- python_scripts/combine_csvs.py
+++ python_scripts/combine_csvs.py
@@ -5,7 +5,11 @@ def get_dir_list():
     for i in range(0,7):
         dir_list.append('belief_uniform_baseline_' + repr(i) + '_reward100_penalty10/simulator/fixed_distribution/')
     #dir_list.append('belief_uniform_cylinder_7_8_9_reward100_penalty10/use_discretized_data/simulator/fixed_distribution/')
-    dir_list.append('belief_uniform_g3db_instances_train1_reward100_penalty10/use_discretized_data/use_weighted_belief/simulator/fixed_distribution/horizon90/')
+    dir_list.append('belief_uniform_g3db_instances_train1_reward100_penalty10/use_discretized_data/use_weighted_belief/fixed_distribution/horizon90/t5_n160/')
+    dir_list.append('belief_uniform_g3db_instances_train1_reward100_penalty10/use_discretized_data/use_weighted_belief/simulator/fixed_distribution/horizon90/t5_n160/')
+    dir_list.append('belief_uniform_g3db_instances_train1_reward100_penalty10/use_discretized_data/fixed_distribution/horizon90/t5_n320/')
+    dir_list.append('belief_uniform_g3db_instances_train1_reward100_penalty10/use_discretized_data/simulator/fixed_distribution/horizon90/t5_n320/')
+    
     return dir_list
 
 def get_grasping_ros_mico_path(server_name = 'ncl'):
@@ -28,7 +32,7 @@ def main():
     
     dir_list = get_dir_list()
     #patterns = ['g3db_instances|', 'all_cylinders|']
-    patterns = ['g3db_instances_train1|', 'g3db_instances_validation1|', 'g3db_instances_test|', 'g3db_instances#']
+    patterns = ['g3db_instances_train1|', 'g3db_instances_validation1|'] #, 'g3db_instances_test|', 'g3db_instances#']
     out_dir = './unicorn_csv_files/grasping_ros_mico/results/despot_logs/low_friction_table/vrep_scene_ver6/multiObjectType/basline_and_cylinder_belief'
     pdr.PROBLEM_NAME = "vrep"
     for pattern in patterns:
